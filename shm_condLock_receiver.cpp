1a74538029f27a950e90ea395085863e3b5b0851
tratitude <tratitude@gmail.com>
2020-04-12 00:04:11 +0200

    add receiver open sample

--- shm_condLock_receiver.cpp
+++ shm_condLock_receiver.cpp
@@ -1,10 +1,12 @@
+#include <unistd.h>
 #include <iostream>
 #include <boost/interprocess/sync/scoped_lock.hpp>
 #include "ShmConfig.hpp"
 
 void print(ShmConfig::Gesture *gesture)
 {
-    while(true){
+    int cnt = 0;
+    while(cnt < 10){
         if(!gesture){
             std::cout << "gesture failed\n";
         }
@@ -17,6 +19,7 @@ void print(ShmConfig::Gesture *gesture)
             
             // receive data
             std::cout << *gesture << std::endl;
+            ++cnt;
 
             // Notify the other process that the buffer is empty
             gesture->condFull.notify_one();
@@ -28,23 +31,35 @@ void print(ShmConfig::Gesture *gesture)
 }
 int main()
 {
-    // remove shmName before usage and after usage
-    struct ShmPreventer{
-        ShmPreventer(){boost::interprocess::shared_memory_object::remove(ShmConfig::shmName);}
-        ~ShmPreventer(){boost::interprocess::shared_memory_object::remove(ShmConfig::shmName);}
-    }shmPreventer;
+    pid_t pid;
 
-    // Create a new segment with given name and size
-    boost::interprocess::managed_shared_memory segment(
-        boost::interprocess::open_or_create, ShmConfig::shmName, ShmConfig::shmSize);
+    pid = fork();
 
-    // Construct an variable in shared memory
-    ShmConfig::Gesture *gesture = segment.construct<ShmConfig::Gesture>(
-        ShmConfig::shmbbCenterGestureName)();
+    if(pid == 0){
+        system("cd ../.. && sh runHandTrackingGPU.sh");
+    }
+    else if(pid > 0){
+        // remove shmName before usage and after usage
+        struct ShmPreventer{
+            ShmPreventer(){boost::interprocess::shared_memory_object::remove(ShmConfig::shmName);}
+            ~ShmPreventer(){boost::interprocess::shared_memory_object::remove(ShmConfig::shmName);}
+        }shmPreventer;
 
-    print(gesture);
+        // Create a new segment with given name and size
+        boost::interprocess::managed_shared_memory segment(
+            boost::interprocess::open_or_create, ShmConfig::shmName, ShmConfig::shmSize);
 
-    segment.destroy<ShmConfig::Gesture>(ShmConfig::shmbbCenterGestureName);
+        // Construct an variable in shared memory
+        ShmConfig::Gesture *gesture = segment.construct<ShmConfig::Gesture>(
+            ShmConfig::shmbbCenterGestureName)();
+
+        print(gesture);
+
+        segment.destroy<ShmConfig::Gesture>(ShmConfig::shmbbCenterGestureName);
+    }
+    else{
+        std::cout << "fork error\n";
+    }
 
     return 0;
 }
